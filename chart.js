32fc5dd414d66f9c201a77c675822d7723ba54bd
Dani Gergely <gdani.y2k@gmail.com>
2021-09-08 20:28:47 +0200

    customizing

--- chart.js
+++ chart.js
@@ -1,24 +1,26 @@
-const screenWidth = process.stdout.columns;
+const chalk = require('chalk');
+
 const screenHeight = process.stdout.rows;
 
 class chart {
-	constructor(widthPercentage, heightPercentage) {
-		this.width = (widthPercentage / 100) * screenWidth;
+	constructor(heightPercentage = 100, chartCharacter = 'o', backgroundCharacter = ' ', chartColor = 'cyan', backgroundColor = 'black') {
 		this.height = (heightPercentage / 100) * screenHeight;
+		this.chartCharacter = chartCharacter;
+		this.backgroundCharacter = backgroundCharacter;
+		this.chartColor = chartColor;
+		this.backgroundColor = backgroundColor;
 	}
 
 	createLine(percentage) {
-		//const char = '¨';
-		const char = '0';
 		let output = [];
 
 		let maxHeight = this.height * (percentage / 100);
 
 		for (let i = this.height; i > 0; i--) {
 			if (i < maxHeight) {
-				output.push(char);
+				output.push(true);
 			} else if (i >= maxHeight) {
-				output.push(' ');
+				output.push(false);
 			}
 		}
 		return output;
@@ -35,7 +37,12 @@ class chart {
 		let output = '';
 		for (let x = 0; x < graphData[0].length; x++) {
 			for (let i = 0; i < graphData.length; i++) {
-				output += graphData[i][x];
+				if (graphData[i][x] == true) {
+					output += chalk[this.chartColor](this.chartCharacter);
+				} else {
+					output += chalk[this.backgroundColor](this.backgroundCharacter);
+				}
+
 				if (i == graphData.length - 1) {
 					output += '\n';
 				}
